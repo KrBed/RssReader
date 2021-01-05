7e7fb6e1beea1e5a890ccc942acd0e8882757674
Krzysztof Bednarski <bednarski1978@gmail.com>
2021-01-05 13:57:02 +0100

    console.php clean

--- src/console.php
+++ src/console.php
@@ -3,12 +3,8 @@
 
 use KrzysztofBednarskiRekrutacjaHRtec\FeedRSS\Classes\RssSchema;
 
-//use KrzysztofBednarskiRekrutacjaHRtec\FeedRSS\Feed;
 use KrzysztofBednarskiRekrutacjaHRtec\FeedRSS\Main;
 
-//use Spatie\SimpleExcel\SimpleExcelWriter;
-
-// include '../vendor/autoload.php';
 require_once __DIR__ . '../../vendor/autoload.php';
 
 echo "RSS Reader will be executed\n";
@@ -16,15 +12,3 @@ echo "RSS Reader will be executed\n";
 $argv = array_values(array_slice($argv, -3, 3, true));
 
 Main::execute($argv);
-
-<<<<<<< HEAD
-
-=======
->>>>>>> fa213462f5d1b289ca759dd33e05c9ae26348e4c
-
-
-
-
-
-
-
