fa213462f5d1b289ca759dd33e05c9ae26348e4c
Krzysztof Bednarski <bednarski1978@gmail.com>
2021-01-05 12:18:27 +0100

    Update console.php

--- src/console.php
+++ src/console.php
@@ -17,32 +17,6 @@ $argv = array_values($argv);
 
 Main::execute($argv);
 
-//$result = Feed::load('https://blog.nationalgeographic.org/feed/');
-//
-//
-//$arr = $result->toArray();
-//$description = $arr['item'][0]['description'];
-//$pubDate = $arr['item'][0]['pubDate'];
-//$articles = [];
-//foreach ($arr['item'] as $item) {
-//  $article = new RssSchema();
-//  $desc = $item['description'];
-//  $article->setDescription($item['description']);
-//  $article->setCreator($item['dc:creator']);
-//  $article->setLink($item['link']);
-//  $article->setPubDate($item['pubDate']);
-//  $article->setTitle($item['title']);
-//  $articles[] = $article;
-//}
-//$writer = SimpleExcelWriter::create('test.csv');
-//foreach ($articles as $article) {
-//  $writer->addRow(['title'       => $article->getTitle(),
-//                   'pubDate'     => $article->getPubDate(),
-//                   'link'        => $article->getLink(),
-//                   'description' => $article->getDescription(),
-//                   'creator'     => $article->getCreator()]);
-//}
-
 
 
 
